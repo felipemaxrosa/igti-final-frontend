fe4b0729c224152fdf9de0b5f104ce1a7c4dba33
FelipeMaxRosa <felipemax.suporteti@gmail.com>
2020-07-25 17:35:18 -0300

    teste de conexao

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Header() {
   return (
     <div className="center">
-      <h2>Bootcamp Full Stack - Desafio Final</h2>
+      <h2>Bootcamp Full Stack</h2>
+      <h3>Desafio Final</h3>
       <h4>Controle Financeiro</h4>
     </div>
   );
